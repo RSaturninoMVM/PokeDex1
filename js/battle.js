ec1ec11d42e59cb7ffe85b05efb0463636e54089
Ruben <rsaturnino@institutmvm.cat>
2026-05-22 19:20:44 +0200

    Añadiendo mas cosas a hunt y battle

---
+++ js/battle.js
@@ -0,0 +1,11 @@
+document.querySelector(".escapar").addEventListener("click", () => {
+    window.location.href = "hunt.html";
+});
+
+document.querySelector(".perder-demostracion").addEventListener("click", () => {
+    window.location.href = "battle-lose.html";
+});
+
+document.querySelector(".ganar-demostracion").addEventListener("click", () => {
+    window.location.href = "battle-win.html";
+});
