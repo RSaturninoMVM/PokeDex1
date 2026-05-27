c8094a67f8b139d29efa2d419ed239c104fec8ab
Ruben <rsaturnino@institutmvm.cat>
2026-05-27 17:14:53 +0200

    Cambios realizos y pagina de batalla realizada y ajustes menores

--- js/battle.js
+++ js/battle.js
@@ -9,3 +9,67 @@ document.querySelector(".perder-demostracion").addEventListener("click", () => {
 document.querySelector(".ganar-demostracion").addEventListener("click", () => {
     window.location.href = "battle-win.html";
 });
+
+async function pokemonEnemigo() {
+    const id = Math.floor(Math.random() * 151) + 1;
+    
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+
+    const nombre = data.name;
+    const imagen = data.sprites.front_default;
+
+    document.getElementById("pokemon-enemigo").src = imagen;
+    
+
+    const barraVida = document.getElementById("vida-enemigo");
+    barraVida.style.width = "100%";
+    barraVida.dataset.saludActual = 100;
+}
+
+async function pokemonAmigo() {
+    const id = Math.floor(Math.random() * 151) + 1;
+    
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+
+    const nombre = data.name;
+    const imagen = data.sprites.front_default;
+
+    document.getElementById("pokemon-amigo").src = imagen;
+    
+
+    const barraVida = document.getElementById("vida-amigo");
+    barraVida.style.width = "100%";
+    barraVida.dataset.saludActual = 100;
+}
+
+pokemonEnemigo();
+pokemonAmigo();
+
+
+document.querySelector(".atacar").addEventListener("click", () => {
+    const barraEnemigo = document.getElementById("vida-enemigo");
+    let vida = parseInt(barraEnemigo.style.width) || 100;
+    
+    vida -= 20;
+    if (vida <= 0) {
+        window.location.href = "battle-win.html";
+    } else {
+        barraEnemigo.style.width = vida + "%";
+    }
+});
+
+document.querySelector(".capturar").addEventListener("click", () => {
+    const barraEnemigo = document.getElementById("vida-enemigo");
+    let vidaActual = parseInt(barraEnemigo.style.width);
+
+    if (vidaActual <= 50) {
+        window.location.href = "battle-win.html";
+    } else {
+        window.location.href = "battle-lose.html";
+    }
+});
+
+document.querySelector(".cambiar").addEventListener("click", () => {
+});
